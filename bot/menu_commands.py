d1ccf5e5e4b916b633272454b32f476bb491061d
anemptyemptiness <103761193+anemptyemptiness@users.noreply.github.com>
2024-09-28 22:59:09 +0300

    #replaced menu commands

--- bot/menu_commands.py
+++ bot/menu_commands.py
@@ -5,9 +5,9 @@ from aiogram import types
 async def set_default_commands(bot: Bot):
     await bot.set_my_commands([
         types.BotCommand(command="start", description="Главное меню 🏡"),
-        types.BotCommand(command="oferta", description="Оферта 📄"),
-        types.BotCommand(command="rules", description="Правила оформления заказа 🧾"),
         types.BotCommand(command="consultation", description="Индивидуальная консультация 📝"),
         types.BotCommand(command="support", description="Тех. поддержка ⚒"),
         types.BotCommand(command="site", description="Сайт основателя техники 🔗"),
+        types.BotCommand(command="rules", description="Правила оформления заказа 📝"),
+        types.BotCommand(command="oferta", description="Оферта 📜"),
     ])
