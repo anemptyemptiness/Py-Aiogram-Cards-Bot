c6232a7e0c69dedf39f76b4463a0d931f3c5f77d
anemptyemptiness <103761193+anemptyemptiness@users.noreply.github.com>
2024-10-05 18:20:40 +0300

    Update init-user-db.sh

--- docker/init-user-db.sh
+++ docker/init-user-db.sh
@@ -2,6 +2,7 @@
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   CREATE ROLE anempty WITH PASSWORD '739154qq';
+  ALTER ROLE anempty WITH LOGIN;
   CREATE DATABASE kryona;
   ALTER DATABASE kryona OWNER TO anempty;
   GRANT ALL PRIVILEGES ON DATABASE kryona TO anempty;
